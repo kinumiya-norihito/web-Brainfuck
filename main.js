c4cbbd990038afde3d9f8ae8b2c9bdc3c6481342
kinumiya-norihito <99883931+kinumiya-norihito@users.noreply.github.com>
2022-11-11 18:51:48 +0900

    add button

--- main.js
+++ main.js
@@ -1,174 +1,178 @@
 window.onload = () => {
 	let
-	bftape,
-	tp,
-	bfcode,
-	cp,
-	lv,
-	strin,
-	intervalId;
+		bftape,
+		tp,
+		bfcode,
+		cp,
+		lv,
+		strin,
+		intervalId;
 	const
-	//定数など
-	//element
-	maxRunInput = document.getElementById('maxRunInput'),
-	bfcodeArea = document.getElementById('bfcodeArea'),
-	outArea = document.getElementById('outArea'),
-	showInfoArea = document.getElementById('showInfoArea'),
-	runButton = document.getElementById('runButton'),
-	stepButton = document.getElementById('stepButton'),
-	resetButton = document.getElementById('resetButton'),
-	cycleButton = document.getElementById('cycleButton'),
-	cpsInput = document.getElementById('cpsInput'),
-	nocInput = document.getElementById('nocInput'),
-	novcInput = document.getElementById('novcInput'),
-	//関数
-	run = (n) => {
-		const f = () => {
-			switch(bfcode[cp]){
-				case '[':
-					lv++;
-					break;
-				case ']':
-					lv--;
-			}
-		};
-		for(let i = 0; i < n; i++){
-			if(tp<0)return;
-			if(!bfcode[cp])return;
-			bftape[tp] = bftape[tp] || 0;
-			switch(bfcode[cp]){
-				case '+':
-					bftape[tp]++;
-					bftape[tp]%=256;
-					break;
-				case '-':
-					bftape[tp]--;
-					if(bftape[tp] < 0)bftape[tp]+=256;
-					break;
-				case '>':
-					tp++;
-					break;
-				case '<':
-					tp--;
-					break;
-				case '[':
-					if(bftape[tp]==0){
+		//定数など
+		//element
+		maxRunInput = document.getElementById('maxRunInput'),
+		bfcodeArea = document.getElementById('bfcodeArea'),
+		outArea = document.getElementById('outArea'),
+		showInfoArea = document.getElementById('showInfoArea'),
+		runButton = document.getElementById('runButton'),
+		stepButton = document.getElementById('stepButton'),
+		resetButton = document.getElementById('resetButton'),
+		cycleButton = document.getElementById('cycleButton'),
+		cpsInput = document.getElementById('cpsInput'),
+		nocInput = document.getElementById('nocInput'),
+		novcInput = document.getElementById('novcInput'),
+		bfco = document.getElementById('bfco'),
+		//関数
+		run = (n) => {
+			const f = () => {
+				switch (bfcode[cp]) {
+					case '[':
 						lv++;
-						while(lv>0){
-							if(bfcode[++cp]){
+						break;
+					case ']':
+						lv--;
+				}
+			};
+			for (let i = 0; i < n; i++) {
+				if (tp < 0) return;
+				if (!bfcode[cp]) return;
+				bftape[tp] = bftape[tp] || 0;
+				switch (bfcode[cp]) {
+					case '+':
+						bftape[tp]++;
+						bftape[tp] %= 256;
+						break;
+					case '-':
+						bftape[tp]--;
+						if (bftape[tp] < 0) bftape[tp] += 256;
+						break;
+					case '>':
+						tp++;
+						break;
+					case '<':
+						tp--;
+						break;
+					case '[':
+						if (bftape[tp] == 0) {
+							lv++;
+							while (lv > 0) {
+								if (bfcode[++cp]) {
+									f();
+								}
+								else {
+									return;
+								}
+							}
+						}
+						break;
+					case ']':
+						lv--;
+						while (lv < 0) {
+							if (--cp > 0) {
 								f();
 							}
-							else{
+							else {
 								return;
 							}
 						}
-					}
-					break;
-				case ']':
-					lv--;
-					while(lv<0){
-						if(--cp>0){
-							f();
+						cp--;
+						break;
+					case '.':
+						outArea.value = outArea.value.replace(/%/g, '%25');
+						outArea.value += '%' + ((256 + bftape[tp]).toString(16).substr(1, 2));
+						try {
+							outArea.value = decodeURIComponent(outArea.value);
 						}
-						else{
-							return;
+						catch (e) { }
+						break;
+					case ',':
+						while (!strin) {
+							strin += prompt('\'%NN\'とすることで0-255までの数値も入力可能\nただし\'%\'を入力する場合は\'%25\'と入力') || '';
+							strin = encodeURI(strin).replace(/%25/g, '%');
 						}
-					}
-					cp--;
-					break;
-				case '.':
-					outArea.value = outArea.value.replace(/%/g,'%25');
-					outArea.value += '%'+((256+bftape[tp]).toString(16).substr(1,2));
-					try{
-						outArea.value = decodeURIComponent(outArea.value);
-					}
-					catch(e){}
-					break;
-				case ',':
-					while(!strin){
-						strin += prompt('\'%NN\'とすることで0-255までの数値も入力可能\nただし\'%\'を入力する場合は\'%25\'と入力') || '';
-						strin = encodeURI(strin).replace(/%25/g,'%');
-					}
-					if(strin[0]=='%'){
-						bftape[tp] = parseInt(strin.substr(1,2),16);
-						strin = strin.substr(3);
-					}
-					else{
-						bftape[tp] = strin.charCodeAt(0);
-						strin = strin.substr(1);
-					}
-					break;
-				case ':':
-					cp++;
-					intervalId&&clearInterval(intervalId);
-					return;
+						if (strin[0] == '%') {
+							bftape[tp] = parseInt(strin.substr(1, 2), 16);
+							strin = strin.substr(3);
+						}
+						else {
+							bftape[tp] = strin.charCodeAt(0);
+							strin = strin.substr(1);
+						}
+						break;
+					case ':':
+						cp++;
+						intervalId && clearInterval(intervalId);
+						return;
+				}
+				cp++;
 			}
-			cp++;
-		}
-	},
-	showInfo = () => {
-		//ここに色々書く
-		showInfoArea.innerHTML = '';
-		for(let j = 0; j < bfcode.length; j++){
-			showInfoArea.innerHTML += (j==cp-1?'<b>':'')+bfcode[j].replace(/\&|\"|\'|\<|\>/g,(match)=>{switch(match){case '&':return '&amp;';case '"':return '&quot;';case '\'':return '&#039;';case '<':return '&lt;';case '>':return '&gt;';}})+(j==cp-1?'</b>':'')+(j%64==63?'<br/>':'');
-		}
-		showInfoArea.innerHTML += '<br/><br/>[';
-		for(let j = 0; j < bftape.length; j++){
-			showInfoArea.innerHTML += (j?',':'')+(j==tp?'<b>':'')+(bftape[j]+256).toString(16).substr(1,2)+(j==tp?'</b>':'');
-		}
-		showInfoArea.innerHTML += `]`;
-	},
-	reset = () => {
-		tp = cp = lv = 0;
-		bftape = new Array();
-		bfcode = bfcodeArea.value;
-		strin = '';
-		outArea.value = '';
-		showInfoArea.innerHTML = '';
-		nocInput.innerHTML = bfcode.length;
-		novcInput.innerHTML = bfcode.replace(/[^+,\-.<>\[\]]+/g,'').length;
-		intervalId&&clearInterval(intervalId);
-		showInfo();
-	};
+		},
+		showInfo = () => {
+			//ここに色々書く
+			showInfoArea.innerHTML = '';
+			for (let j = 0; j < bfcode.length; j++) {
+				showInfoArea.innerHTML += (j == cp - 1 ? '<b>' : '') + bfcode[j].replace(/\&|\"|\'|\<|\>/g, (match) => { switch (match) { case '&': return '&amp;'; case '"': return '&quot;'; case '\'': return '&#039;'; case '<': return '&lt;'; case '>': return '&gt;'; } }) + (j == cp - 1 ? '</b>' : '') + (j % 64 == 63 ? '<br/>' : '');
+			}
+			showInfoArea.innerHTML += '<br/><br/>[';
+			for (let j = 0; j < bftape.length; j++) {
+				showInfoArea.innerHTML += (j ? ',' : '') + (j == tp ? '<b>' : '') + (bftape[j] + 256).toString(16).substr(1, 2) + (j == tp ? '</b>' : '');
+			}
+			showInfoArea.innerHTML += `]`;
+		},
+		reset = () => {
+			tp = cp = lv = 0;
+			bftape = new Array();
+			bfcode = bfcodeArea.value;
+			strin = '';
+			outArea.value = '';
+			showInfoArea.innerHTML = '';
+			nocInput.innerHTML = bfcode.length;
+			novcInput.innerHTML = bfcode.replace(/[^+,\-.<>\[\]]+/g, '').length;
+			intervalId && clearInterval(intervalId);
+			showInfo();
+		};
 
 	reset();
 
 	//イベント
-	maxRunInput.addEventListener('blur',()=>{
+	maxRunInput.addEventListener('blur', () => {
 		const x = +maxRunInput.value || 0;
-		maxRunInput.value = x<1?1000000:Math.floor(x);
+		maxRunInput.value = x < 1 ? 1000000 : Math.floor(x);
 	});
-	cpsInput.addEventListener('blur',()=>{
+	cpsInput.addEventListener('blur', () => {
 		const x = +cpsInput.value || 0;
-		cpsInput.value = x<10?10:Math.floor(x);
+		cpsInput.value = x < 10 ? 10 : Math.floor(x);
 	});
-	bfcodeArea.addEventListener('change',()=>{
+	bfcodeArea.addEventListener('change', () => {
 		reset();
 	});
-	runButton.addEventListener('click',()=>{
+	runButton.addEventListener('click', () => {
 		run(+maxRunInput.value);
 		showInfo();
 	});
-	stepButton.addEventListener('click',()=>{
+	stepButton.addEventListener('click', () => {
 		run(1);
 		showInfo();
 	});
-	resetButton.addEventListener('click',()=>{
+	resetButton.addEventListener('click', () => {
 		reset();
 	});
-	cycleButton.addEventListener('click',()=>{
+	bfco.addEventListener('click', () => {
+		bfcodeArea.value = bfcode.replace(/[^+,\-.<>\[\]:]+/g, '')
+	});
+	cycleButton.addEventListener('click', () => {
 		let i = 0;
 		const
-		lim = +maxRunInput.value,
-		cps = +cpsInput.value;
+			lim = +maxRunInput.value,
+			cps = +cpsInput.value;
 
-		intervalId&&clearInterval(intervalId);
-		intervalId = setInterval(()=>{
+		intervalId && clearInterval(intervalId);
+		intervalId = setInterval(() => {
 			run(1);
 			showInfo();
-			if(i==lim){
+			if (i == lim) {
 				clearInterval(intervalId);
 			}
-		},cps);
+		}, cps);
 	});
 };
